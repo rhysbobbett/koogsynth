2eb26ce25a6010ea53b37e5ef1ada792eef0751a
rhysbobbett <rhys000bobbett@googlemail.com>
2023-06-28 17:51:59 +0000

    Added all functionality to LFO Rate, LFO q

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,29 @@
+// Audio Context and Nodes
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const vcaOscillator = audioContext.createOscillator();
+const vcaOscillatorGain = audioContext.createGain();
+const lfoOscillator = audioContext.createOscillator();
+const lfoOscillatorGain = audioContext.createGain();
 
-waveformSelectVCO.addEventListener('click', () => {
-  waveformSelectVCO.checked = true;
-  waveformSelectVCF.checked = false;
-  waveformSelectVCOVCF.checked = false;
-});
+// Power Button
+const powerButton = document.getElementById('onoff');
 
-waveformSelectVCF.addEventListener('click', () => {
-  waveformSelectVCO.checked = false;
-  waveformSelectVCF.checked = true;
-  waveformSelectVCOVCF.checked = false;
-});
+// VCA Oscillator and Gain Control
+const vcaGainSlider = document.getElementById('vcagain');
+const vcaWaveformSelect = document.getElementById('waveformVcaOscillator');
 
-waveformSelectVCOVCF.addEventListener('click', () => {
-  waveformSelectVCO.checked = false;
-  waveformSelectVCF.checked = false;
-  waveformSelectVCOVCF.checked = true;
-});
+// LFO Oscillator and Gain Control
+const lfoFrequencySlider = document.getElementById('lforate');
+const lfoIntensitySlider = document.getElementById('lfopeak');
+const lfoWaveformSelect = document.getElementById('waveformLfoOscillator');
 
+// Octave Control
+const octaveSlider = document.getElementById('octave-slider');
+let currentOctave = parseInt(octaveSlider.value);
 
-let dials = [];
-
-dials[0] = new Knob({
-  id: "vcfcutoff",
-  size: "medium",
-  type: "LittlePhatty",
-  lowVal: 0,
-  highVal: 100,
-  value: 50,
-  sensitivity: 0.9,
-  onChange: (val) => knobChanged("vcfcutoff", val),
-});
-
-dials[1] = new Knob({
-  id: "vcfpeak",
-  size: "medium",
-  type: "LittlePhatty",
-  lowVal: 0,
-  highVal: 100,
-  value: 50,
-  sensitivity: 0.9,
-  onChange: (val) => knobChanged("vcfpeak", val),
-});
-
-dials[2] = new Knob({
-  id: "lforate",
-  size: "medium",
-  type: "LittlePhatty",
-  lowVal: 0,
-  highVal: 100,
-  value: 50,
-  sensitivity: 0.9,
-  onChange: (val) => knobChanged("lforate", val),
-});
-
-dials[3] = new Knob({
-  id: "lfoint",
-  size: "medium",
-  type: "LittlePhatty",
-  lowVal: 0,
-  highVal: 100,
-  value: 50,
-  sensitivity: 0.9,
-  onChange: (val) => knobChanged("lfoint", val),
-});
-
-dials[4] = new Knob({
-  id: "noise",
-  size: "medium",
-  type: "LittlePhatty",
-  lowVal: 0,
-  highVal: 100,
-  value: 50,
-  sensitivity: 0.9,
-  onChange: (val) => knobChanged("noise", val),
-});
-
-dials[5] = new Knob({
-  id: "VCAGain",
-  size: "medium",
-  type: "LittlePhatty",
-  lowVal: 0,
-  highVal: 100,
-  value: 50,
-  sensitivity: 0.9,
-  onChange: (val) => knobChanged("noise", val),
-});
-
-// Knob parameter console log
-function knobChanged(id, val) {
-  console.log(`Knob with ID: ${id} changed to ${val}`);
-}
-
-let octave = 4;
+// Note-to-Pitch and Note-to-Key Mappings
 const noteToPitch = {
-  'F': 43.653528929125485,
+  'F1': 43.653528929125485,
   'F#': 46.249302838954299,
   'G': 48.999429497718661,
   'G#': 51.913087197493142,
@@ -107,200 +37,129 @@ const noteToPitch = {
   'E': 82.41,
   'F2': 87.31,
 };
-
-
-
-
-// event listeners
-window.addEventListener('keydown', handleKeyDown);
-window.addEventListener('keyup', handleKeyUp);
-
-// Synth power //
-function synthOnOff() {
-  if (synthOn) {
-    audioContext.close();
-    synthOn = false;
-    document.getElementById("onoff").value = "Off";
-  }
-  else {
-    if (createInstrument()) { // ensure that it succeeded
-      synthOn = true;
-      document.getElementById("onoff").value = "On";
-    }
-  }
-}
-
-// Mouse Listener for keyboard click //
-const keyboard = document.getElementById('keyboard');
-const keys = keyboard.querySelectorAll('.white-key, .black-key');
-const pressedKeys = {};
-
-keys.forEach(key => {
-  key.addEventListener('mousedown', () => {
-    const note = key.dataset.note;
-    playNote(note);
-    pressedKeys[note] = true;
-  });
-
-  key.addEventListener('mouseup', () => {
-    const note = key.dataset.note;
-    stopNote(note);
-    delete pressedKeys[note];
-  });
-
-  key.addEventListener('mouseleave', () => {
-    const note = key.dataset.note;
-    if (pressedKeys[note]) {
-      stopNote(note);
-      delete pressedKeys[note];
-    }
-  });
-});
-
-function handleKeyDown(event) {
-  if (event.repeat) return;
-
-  const key = event.key.toLowerCase();
-  const note = getNoteFromKey(key);
-
-  if (note) {
-    playNote(note);
-  }
-
-  // Octave control key handler
-  if (key === 'z') {
-    decreaseOctave();
-  } else if (key === 'x') {
-    increaseOctave();
-  }
-}
-
-function handleKeyUp(event) {
-  const key = event.key.toLowerCase();
-  const note = getNoteFromKey(key);
-
-  if (note) {
-    stopNote(note);
-  }
-}
-
-// Octave Control Logic
-function decreaseOctave() {
-  if (octave > 0) {
-    octave--;
-    octaveSlider.value = octave;
-  }
-}
-
-function increaseOctave() {
-  if (octave < 8) {
-    octave++;
-    octaveSlider.value = octave;
-  }
-}
-
-// Letter to Note Assignment //
-function getNoteFromKey(key) {
-  const keyboardMap = {
-    a: 'F',
-    w: 'F#',
-    s: 'G',
-    e: 'G#',
-    d: 'A',
-    r: 'A#',
-    f: 'B',
-    g: 'C',
-    y: 'C#',
-    h: 'D',
-    u: 'D#',
-    j: 'E',
-    k: 'F2',
-  };
-
-  return keyboardMap[key];
-
-}
-
-// Oscillator Note playback
-function playNote(note) {
-  const frequency = getFrequency(note);
-  const audioCtx = new AudioContext();
-  
-  if (toggleButtonVCOVCF.checked) {
-    const vcoVcfWaveform = waveformSelectVCO.value;
-    const vcoVcfCutoff = dials[0].getValue() * 10;
-    const vcoVcfPeak = dials[1].getValue() * 10;
-    const lfoVcfRate = dials[2].getValue() * 10;
-    const lfoVcfInt = dials[3].getValue() * 10;
-
-    vcoVcfOscillator.start();
-    
-    oscillators[note] = [vcoVcfOscillator, vcfOscillator, lfoOscillator, lfoVcfModu, ];
-  } 
-  
-  else {
-    const vcfWaveform = waveformSelectVCF.value;
-    const vcfCutoff = dials[0].getValue() * 10;
-    const vcfPeak = dials[1].getValue() * 10;
-    const vcfOscillator = createOscillator(vcfWaveform, frequency, vcfCutoff, vcfPeak);
-    const vcoWaveform = waveformSelectVCO.value;
-    const vcoCutoff = dials[1].getValue() * 10;
-    const vcoOscillator = createOscillator(vcoWaveform, frequency, vcoCutoff);
-    vcoOscillator.start();
-    vcfOscillator.start();
-
-    oscillators[note] = [vcoOscillator, vcfOscillator];
-  }
-  function createOscillator(waveform, frequency, cutoff) {
-    const oscillator = audioContext.createOscillator();
-    const vcf = audioContext.createBiquadFilter();
-    oscillator.type = waveform;
-    vcf.type = "oscillator";
-
-    oscillator.frequency.setValueAtTime(frequency, audioContext.currentTime);
-    vcf.frequency.setValueAtTime(cutoff, audioContext.currentTime);
-
-    oscillator.connect(vcf).connect(audioContext.destination);
-
-    if (!lfo) {
-      lfo = audioContext.createOscillator();
-      lfo.type = waveformSelectVCO.value;
-      lfo.frequency.setValueAtTime(dials[3].getValue() * 100, audioContext.currentTime);
-      lfo.start();
-    }
-
-    oscillator.connect(vcf.frequency);
-    lfo.connect(vcf.frequency);
-
-    return oscillator;
-  }
-}
-
-function stopNote(note) {
-  const oscillatorsToStop = oscillators[note];
-  if (oscillatorsToStop) {
-    oscillatorsToStop.forEach(oscillator => {
-      oscillator.stop();
-      oscillator.disconnect();
-    });
-    delete oscillators[note];
-  }
-}
-
-function getFrequency(note) {
-  const octaveMultiplier = Math.pow(2, octave);
-  return noteToPitch[note] * octaveMultiplier;
-}
-
-octaveSlider.addEventListener('input', () => {
-  octave = parseInt(octaveSlider.value);
-});
-
-function knobChanged(id, val) {
-  console.log(`Knob with ID: ${id} changed to ${val}`);
+const keyToNote = {
+  'A': 'F1',
+  'W': 'F#',
+  'S': 'G',
+  'E': 'G#',
+  'D': 'A',
+  'R': 'A#',
+  'F': 'B',
+  'G': 'C',
+  'Y': 'C#',
+  'H': 'D',
+  'U': 'D#',
+  'J': 'E',
+  'K': 'F2',
 };
 
+// Play and Stop Note Functions
+const activeNotes = {};
 
+function playSound(note) {
+  if (!activeNotes[note]) {
+    const frequency = noteToPitch[note] * Math.pow(2, currentOctave - 4);
+    const vcaOscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    const lfoMod = audioContext.createOscillator();
+    const lfoModGain = audioContext.createGain();
 
+    lfoMod.frequency.value = parseFloat(lfoFrequencySlider.value);
+    lfoMod.type = lfoWaveformSelect.value;
+    lfoModGain.gain.value = parseFloat(lfoIntensitySlider.value);
+    lfoMod.connect(lfoModGain);
 
+    vcaOscillator.type = vcaWaveformSelect.value;
+    vcaOscillator.frequency.value = frequency;
+    vcaOscillator.connect(gainNode);
+    gainNode.gain.value = parseFloat(vcaGainSlider.value);
+    gainNode.connect(audioContext.destination);
 
+    activeNotes[note] = { vcaOscillator, gainNode, lfoMod, lfoModGain };
+
+    lfoModGain.connect(vcaOscillator.frequency);
+
+    vcaOscillator.start();
+    lfoMod.start();
+  }
+}
+
+function stopSound(note) {
+  if (activeNotes[note]) {
+    const { vcaOscillator, gainNode, lfoMod } = activeNotes[note];
+    vcaOscillator.stop();
+    lfoMod.stop();
+    gainNode.disconnect();
+    lfoMod.disconnect();
+    delete activeNotes[note];
+  }
+}
+
+// Event Listeners
+powerButton.addEventListener('change', function () {
+  if (powerButton.checked) {
+    audioContext.resume();
+  } else {
+    audioContext.suspend();
+  }
+});
+
+vcaGainSlider.addEventListener('input', function () {
+  const gainValue = parseFloat(vcaGainSlider.value);
+  for (const note in activeNotes) {
+    activeNotes[note].gainNode.gain.setValueAtTime(gainValue, audioContext.currentTime);
+  }
+});
+
+lfoFrequencySlider.addEventListener('input', function () {
+  const frequencyValue = parseFloat(lfoFrequencySlider.value);
+  for (const note in activeNotes) {
+    activeNotes[note].lfoMod.frequency.setValueAtTime(frequencyValue, audioContext.currentTime);
+  }
+});
+
+lfoIntensitySlider.addEventListener('input', function () {
+  const intensityValue = parseFloat(lfoIntensitySlider.value);
+  for (const note in activeNotes) {
+    activeNotes[note].lfoModGain.gain.setValueAtTime(intensityValue, audioContext.currentTime);
+  }
+});
+
+octaveSlider.addEventListener('input', function () {
+  currentOctave = parseInt(octaveSlider.value);
+});
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'z') {
+    if (currentOctave > 0) {
+      currentOctave--;
+      octaveSlider.value = currentOctave;
+    }
+    event.preventDefault();
+  } else if (event.key === 'x') {
+    if (currentOctave < 8) {
+      currentOctave++;
+      octaveSlider.value = currentOctave;
+    }
+    event.preventDefault();
+  } else {
+    const note = keyToNote[event.key.toUpperCase()];
+    if (note && !activeNotes[note]) {
+      playSound(note);
+    }
+  }
+});
+
+document.addEventListener('keyup', function (event) {
+  if (event.key === 'z' || event.key === 'x') {
+    event.preventDefault();
+  }
+  const note = keyToNote[event.key.toUpperCase()];
+  if (note) {
+    stopSound(note);
+  }
+});
+
+// Connect Nodes
+vcaOscillator.connect(vcaOscillatorGain);
+vcaOscillatorGain.connect(audioContext.destination);
